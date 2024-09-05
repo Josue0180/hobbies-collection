3dc6562e389363d8aacd1caba32781ec817d0122
Josue0180 <jooficial14@gmail.com>
2024-09-05 12:02:35 -0300

    qualquer coisa

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_URL ='http://127.0.0.1:5000/api/livros'
+API_URL = 'http://127.0.0.1:5000/api/livros'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -12,8 +12,8 @@ def index():
         autor = request.form.get('autor')
 
         response = requests.post(API_URL, json={'titulo':titulo, 'autor': autor})
-
-    response = requests.get(API_URL)        
+    
+    response = requests.get(API_URL)
     if response.status_code == 200:
         livros = response.json()
     else:
@@ -22,4 +22,4 @@ def index():
     return render_template('index.html', livros=livros)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug= True)
+    app.run(port=5001, debug=True)
